1b086ce15e64856062ac604db11d065e130e8c52
Dovah Viking <DovahViking@hotmail.com>
2020-05-05 10:15:21 +0200

    ?!

--- Program.cs
+++ Program.cs
@@ -6,6 +6,25 @@ namespace Bannerlord_Console
     {
         static void Main(string[] args)
         {
+            bool quit = false;
+            while (!quit)
+            {
+                ConsoleKeyInfo input = Console.ReadKey(true);
+
+                if (input.Key == ConsoleKey.D1)
+                {
+                    Console.WriteLine("1aaaaaaaaaaaaa");
+                }
+                if (input.Key == ConsoleKey.D2)
+                {
+                    Console.WriteLine("2bbbbbbbbbbbb");
+                }
+                if (input.Key == ConsoleKey.Q)
+                {
+                    quit = true;
+                }
+            }
+
             UI user_interface = new UI();
             user_interface.User_Interface();
         }
